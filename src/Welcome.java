6751de17d5c3b6c3d14a626156789088d9365e3b
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-07-03 13:05:59 +0700

    week1 tutorial

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,5 +1,39 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("println1");
+        System.out.print("print");
+        System.out.println("/println2");
+        System.out.println(3 % 5);
+
+        System.out.println(10.0 / 3.0 + 5.0 * 2.0);
+        System.out.println(10 - 3 % 5 * 2);
+        System.out.println(10 / 3 + 5 / 2);
+        System.out.println(13 % 5 / 2);
+        System.out.println((10 + 3 / 2) * 3);
+        System.out.println(5.0 % 3.0);
+
     }
 }
+
+
+//comment here
+
+class OneOne
+{
+    public static final int SIZE = 20; // In here final is the constant
+
+    public static void main(String[] args)
+    {
+        String a = new String("Well!");
+        String SecondString = "Now!";
+
+        System.out.println("Isn't this an interesting program");
+                System.out.println("Answer yes or no!");
+    }
+
+    //SIZE = 30; // it declare as final on the above so it is impossible to use it
+}
+
