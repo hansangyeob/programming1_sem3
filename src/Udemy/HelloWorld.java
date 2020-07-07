74354a53f821e298249112485957feafcafc67b2
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-07-07 12:03:38 +0700

    hello world

---
+++ src/Udemy/HelloWorld.java
@@ -0,0 +1,15 @@
+package Udemy;
+
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+
+}
+
+//public is an access modifier defining the class
+// static is a Java keyword that needs an
+// understanding of other concepts
+//void is yet another java keyword used to indicate that method
+//will not return anything more on that later.
