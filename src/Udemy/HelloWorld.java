e9f7a201aa04fffa45bbd172b406f7e788781bc4
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-07-07 14:16:13 +0700

    add byte file

--- src/Udemy/HelloWorld.java
+++ src/Udemy/HelloWorld.java
@@ -1,15 +1,39 @@
+/*
+1. public is an access modifier defining the class
+2. static is a Java keyword that needs an understanding of other concepts
+3. void is yet another java keyword used to indicate that method will not return anything more on that later.
+4. Primitive Types are the most basic data types
+  - boolean
+  - byte
+  - char
+  - short
+  - int
+  - long
+  - float
+  - double
+
+
+ */
+
+
 package Udemy;
 
-public class HelloWorld {
+public class HelloWorld{
     public static void main(String[] args){
+        System.out.println("Hello,Tonyy!");
+        System.out.println("Hello!!");
 
-        System.out.println("Hello World");
+        int myFirstNumber = 5; // Defining variable
+        // it is Declaration statement
+        // Expression is a construct that evaluates to a single value
+        int mySecondNumber = 10;
+        int myThirdNumber = 15;
+        int myAllNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastNumber = myAllNumber - 1000;
+        System.out.println(myLastNumber);
+        //sout is short cut
+        // when we put something in "", it is called string literal
+
     }
-
 }
 
-//public is an access modifier defining the class
-// static is a Java keyword that needs an
-// understanding of other concepts
-//void is yet another java keyword used to indicate that method
-//will not return anything more on that later.
---
+++ src/Udemy/Main.java
@@ -0,0 +1,22 @@
+/*
+1. Primitive Types are the most basic data types
+  - boolean
+  - byte
+  - char
+  - short
+  - int
+  - long
+  - float
+  - double
+2. Java packages
+ - a package is a way to organize your java projects.
+*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
