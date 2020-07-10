d0f8815a2f59219c81ea59b26187ed82bd9deee3
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-07-10 13:50:42 +0700

    tutorial2

---
+++ src/operater/src/Udemy/Main.java
@@ -0,0 +1,37 @@
+//operators
+/*
+ operand
+ int myVar = 15+12;
+ 1. "+" is operator
+ 2. 15,12 are operands
+* */
+package Udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+        int result = 1 + 2;
+        System.out.println(result);
+        int previousResult = result;
+        System.out.println("PreviousResult = " + previousResult);
+        result = result - 1;
+        System.out.println(result);
+
+        boolean isAlien = true;
+        if (isAlien == false) {
+            System.out.println("It is not an aline!");
+        }else{
+                System.out.println("It is an aline!!");
+            }
+*/
+
+        int ageOfClient = 20;
+        boolean isEighteenOrOver = ageOfClient == 20? true:false;
+        System.out.println(isEighteenOrOver);
+
+
+    }//2
+}//1
+
+
---
+++ src/week2tutorial/src/week2/Main.java
@@ -0,0 +1,57 @@
+package week2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println(returnLargerNumber(12,14));
+        //System.out.println(studentMark(22));
+        //System.out.println(isItTriangle(4,4,4));
+
+
+    }//main close
+
+    /*
+    //0. Write a static method that accepts 2 numbers. Return the larger number.
+    public static int returnLargerNumber(int a, int b) {
+        if (a > b) {
+            System.out.println(a + " is the larger number than " + b);
+            return a;
+        } else {
+            System.out.println(b + " is the larger than " + a);
+            return b;
+        }
+
+}
+*/
+
+    /*
+    //1. Write a static method to return student grades  (HD, DI, CR, PA or NN).
+    // Input is students marks between 0 and 100.
+  public static String studentMark(int a){
+      if(90<= a && a <=100){
+          return "HD";
+      }else if (70<=a && a<90){
+          return "DI";
+      }else if (60<=a && a<70){
+          return "CR";
+      }else if(50<=a && a<60){
+          return "PA";
+      }else{
+          return "NN";
+      }
+  }
+*/
+
+    /*
+    //2. Write a static method to check if three sides of a triangle is valid. Input: a, b, c
+ public static String isItTriangle(int a,int b, int c){
+     if (a+b>c && b+c>a && a+c>b){
+         return "it is triangle!";
+     }else{
+         return "it is not a triangle!";
+     }
+ }
+ */
+
+}// Main class close
+
