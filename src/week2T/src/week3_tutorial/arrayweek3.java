dc8ea7bc372d427e71d277d67cade8b5ca9fed8e
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-07-24 08:40:32 +0700

    week4T

--- src/week2T/src/week3_tutorial/arrayweek3.java
+++ src/week2T/src/week3_tutorial/arrayweek3.java
@@ -1,4 +1,31 @@
 package week3_tutorial;
 
+import java.util.Arrays;
+
 public class arrayweek3 {
+    public static void main(String[] args) {
+  /*
+        String mess = "new!";
+        System.out.println(mess.endsWith("!!"));
+        System.out.println(mess.length());
+        System.out.println(mess.indexOf("r"));
+        System.out.println(mess.replace("!","??"));
+
+        int [] numbers = {105,212,334,142,857};
+        Arrays.sort(numbers);
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+        int[][] numberMulti = {{1, 2, 3},{4,5,6}};
+        System.out.println(Arrays.deepToString(numberMulti));
+*/
+
+    }
+//7.Write a method that computes the average of the values in an array of doubles.
+// The header of the method is as follows:
+    public static double averages(double[] x){
+        double [] a = {1.1,2,2.3,3};
+        for(int i =0; i==x.length; i++ ){
+
+        }
+    }
 }
