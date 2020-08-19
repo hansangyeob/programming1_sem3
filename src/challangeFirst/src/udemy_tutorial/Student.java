f66c8074e5b3bbf3a2b41ab2aa33a3c7554c1b26
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-08-19 14:28:45 +0700

    tutorial week5

--- src/challangeFirst/src/udemy_tutorial/Student.java
+++ src/challangeFirst/src/udemy_tutorial/Student.java
@@ -1,1 +1,96 @@
+package udemy_tutorial;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Student
+{
+    int id;
+    String name;
+    Date birthDate;
+    String address;
+    int phone;
+    String email;
+
+    public Student() {
+    }
+
+    //Constructor
+    public Student(int id, String name, Date birthDate,  int phone, String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.address = "702 NVL";
+        this.phone = phone;
+        this.email = email;
+    }
+
+    //Getter and Setter:
+    public int getId()
+    {
+        return id;
+    }
+    public void setId()
+    {
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean canRideBike() throws ParseException {
+
+        Date date = new Date();
+
+        int diffInDays = (int)( (date.getTime() - this.getBirthDate().getTime())
+                / (1000 * 60 * 60 * 24) );
+
+        System.out.println(diffInDays);
+
+        if (diffInDays/365>=18) return true;
+        else return false;
+    }
+}
--- src/challangeFirst/src/udemy_tutorial/testingScanner.java
+++ src/challangeFirst/src/udemy_tutorial/testingScanner.java
@@ -1,17 +1,75 @@
 package udemy_tutorial;
+import javax.crypto.spec.PSource;
 import java.util.Scanner;
+import java.util.Date;
+import java.util.Random;
 
 public class testingScanner {
 
     public static void main(String[] args) {
-        Circle thisCircle = new Circle();
+        Date a = new Date();
+        Random model = new Random(500);
 
-        thisCircle.setColor("Red");
-        System.out.println(thisCircle.getColor());
+        System.out.println(a.toString());
+        System.out.println(a.getTime());
+        System.out.println(a);
 
-        thisCircle.setRadius(19.2);
-        System.out.println(thisCircle.getRadius());
-        System.out.println(thisCircle.calculate());
+        System.out.println(model.nextInt(1000));
+
+        try {
+            int[] myNumber = {1, 2, 3};
+            System.out.println(myNumber[2]);
+        } catch (Exception ex) {
+            System.out.println("Something is wrong dude!");
+        } finally {
+            System.out.println("The 'try catch' is finished");
+        }
+        checkAge(19);
+
+        try {
+            for (int i = 0; i < 2; i++) {
+                System.out.println(i + " ");
+                System.out.println(1 / 0);
+            }
+        } catch (Exception ex) {
+            System.out.println("Isnt is Arithmetic error? ");
+        }
+
+        long valueNormal = Long.MAX_VALUE - 1;
+        long value = Long.MIN_VALUE + 1;
+        System.out.println(valueNormal);
+        System.out.println(value);
+
+        try {
+            int valueMine = 50;
+            if (valueMine < 40)
+                throw new Exception("valueMine is too  small”");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("“Continue after the catch block”");
+        try {
+            int [] list = new int[10];
+            System.out.println ("“ list[10]  is”" + list[10]);
+        }
+        catch (ArithmeticException ex) {
+            System.out.println("“Arithmetic Exception”"); }
+        catch (RuntimeException ex) {
+            System.out.println ("“Runtime Exception”"); }
+
+        catch (Exception ex) {
+            System.out.println("“Exception”"); }
     }
 
-  }
+
+    static void checkAge(int age){
+        if( age<18) {
+            throw new ArithmeticException("Access denided");
+        }else {
+            System.out.println("You are old enough");
+        }
+        }
+        }
+
+
+
