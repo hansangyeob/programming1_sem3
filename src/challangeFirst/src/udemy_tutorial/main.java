76b97590d516c6452eacfde3d04d55756839cf64
hansangyeob <57244611+hansangyeob@users.noreply.github.com>
2020-08-24 13:16:12 +0700

    as

--- src/challangeFirst/src/udemy_tutorial/main.java
+++ src/challangeFirst/src/udemy_tutorial/main.java
@@ -1,4 +1,33 @@
 package udemy_tutorial;
+import java.util.Scanner;
 
 public class main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myInteger = getIntegers(5);
+        for(int i = 0; i<myInteger.length; i++){
+            System.out.println("element" + i + ", typed value was"+ myInteger[i]);
+        }
+
+        System.out.println("The average is" + getAverage(myInteger));
+    }
+
+    public static int[] getIntegers(int numbers){
+        System.out.println("Enter"+ numbers+ "integer values.\r");
+        int[] values = new int [numbers];
+        for(int i=0;i< values.length;i++){
+            values [i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for(int i=0; i<array.length; i++){
+            sum += array[i];
+        }
+        return (double) sum/(double)array.length;
+    }
 }
